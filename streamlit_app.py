00e48093d226a74bf4880cae9fec76e592171960
Lucas Rodrigues <lucas.r.ribeiro93@gmail.com>
2025-06-25 19:24:30 +0000

    Atualização do código e melhorias

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,38 +5,59 @@ import pydeck as pdk
 import numpy as np
 from pydeck.types import String
 
-df = pd.read_csv('inmet_filter.csv')
-df['data'] = pd.to_datetime(df['datahora']).dt.date
+df = pd.read_csv('inmet.csv')
+df['data'] = pd.to_datetime(df['data']).dt.date
 primeiro = df['data'].min()
 ultimo = df['data'].max()
-
-
-def TabMedias (dtselect):
-    dfMean = df[(df['data']>=dtselect[0]) & (df['data']<=dtselect[1])].drop(['codEstacao', 'datahora', 'data'], axis=1).groupby(['nomeEstacao', 'latitude', 'longitude']).sum().reset_index()
-    dfMean['valorGrafico'] = (dfMean['nomeEstacao'] + "\n" + dfMean['valorMedida'].round(2).astype(str))
-    return dfMean
 
 st.set_page_config(layout='wide')
 st.title('Histórico de chuva em São Paulo')
 
 left_column, right_column = st.columns([2,1])
 
+def df_Tabela(dtselect):
+    tabela = df.drop(['ano', 'mes', 'latitude', 'longitude'], axis=1)
+    tabela = tabela[(tabela['data']>=dtselect[0]) & (tabela['data']<=dtselect[1])]
+    tabela = tabela.groupby(['nomeEstacao', 'codEstacao', 'coordenadas', 'data']).sum()
+    tabela.rename(columns={'valorMedida':'Precipitação (mm)', 'nomeEstacao':'Nome Estação', 'coordenadas':'Coordenadas'}, inplace=True)
+    return tabela
+
+def df_Pontos(dtselect):
+    tabela2 = df.drop(['ano', 'mes', 'coordenadas'], axis=1)
+    tabela2 = tabela2[(tabela2['data']>=dtselect[0]) & (tabela2['data']<=dtselect[1])].drop(['data'], axis=1)
+    tabela2 = tabela2.groupby(['nomeEstacao', 'latitude', 'longitude']).sum().reset_index()
+    return tabela2
+
 with right_column:
     dtselect = st.date_input("Escolha o período: ", value=(primeiro,ultimo), min_value=primeiro, max_value=ultimo)
-    NewTable = TabMedias(dtselect)
-    NewTable.rename(columns={'valorMedida':'Precipitação (mm)', 'nomeEstacao':'Nome Estação', 'latitude':'Latitude', 'longitude':'Longitude'}, inplace=True)
-    st.dataframe(NewTable.drop(["valorGrafico"], axis=1), use_container_width=True)
+    st.dataframe(df_Tabela(dtselect))
 
-TextLayer_ = pdk.Layer(
-                "TextLayer",
-                data=TabMedias(dtselect)[['latitude', 'longitude', 'nomeEstacao', 'valorGrafico']],
+RaioAcao = pdk.Layer(
+                "ScatterplotLayer",
+                data=df_Pontos(dtselect),
+                pickable=True,
+                stroked=False,
+                filled=True,
+                radius_scale=1,
+                line_width_min_pixels=1,
                 get_position=['longitude','latitude'],
-                get_text='valorGrafico',
-                get_size=13,
-                get_color=[0,0,0],
-                background=True,
-                get_background_color=[21, 124, 179],
-                background_padding=[5,5,5,5]
+                get_radius=1000,
+                get_fill_color=[25, 150, 217, 175],
+            )
+Ponto = pdk.Layer(
+                "ScatterplotLayer",
+                data=df_Pontos(dtselect),
+                pickable=True,
+                opacity=1,
+                stroked=False,
+                filled=True,
+                radius_scale=1,
+                radius_min_pixels=5,
+                radius_max_pixels=10,
+                line_width_min_pixels=1,
+                get_position=['longitude','latitude'],
+                get_radius=10,
+                get_fill_color=[255, 75, 75],
             )
 
 with left_column:
@@ -49,7 +70,21 @@ with left_column:
                 zoom=5.5,
                 #pitch=50,
             ),
-            layers=[TextLayer_],
-            tooltip={"html": "<b>Elevation Value:</b> {elevationValue} <br/> <b>Color Value:</b> {colorValue}","style": {"backgroundColor": "steelblue", "color": "white"}}            
+            layers=[RaioAcao,Ponto],
+            tooltip={"html": "<b>Estação:</b> {nomeEstacao} <br/> <b>Precipitação:</b> {valorMedida}","style": {"backgroundColor": "steelblue", "color": "white"}}            
         )
     )
+
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+                #deckgl-wrapper {
+                    height: 85vh !important
+                }
+        </style>
+        """, unsafe_allow_html=True)
